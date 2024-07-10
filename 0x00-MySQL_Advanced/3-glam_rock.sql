eb328c6839595d817c462bc2e5b4fb5261f3731a
ifeanyi zion chidiebere <ifeanyizion3@gmail.com>
2024-07-10 20:03:49 +0000

    task #3: custom sql function

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- Lists all bands with 'Glam rock' as their main style
+-- ranked by longevity
+
+SELECT band_name,
+    CASE
+	WHEN split IS NULL THEN ('2020' - formed)
+	ELSE split - formed
+    END AS lifespan
+
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', style) > 0
+    ORDER BY lifespan DESC;
