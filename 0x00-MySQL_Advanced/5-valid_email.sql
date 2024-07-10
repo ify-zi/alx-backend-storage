0519e4011761d642c0f52fa3a123531e02ce93b5
ifeanyi zion chidiebere <ifeanyizion3@gmail.com>
2024-07-10 20:37:10 +0000

    task #5: still on trigger with update clasue

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,8 +8,8 @@ CREATE TRIGGER email_var AFTER UPDATE ON users
 	FOR EACH ROW
 	BEGIN
 		IF NEW.email <> OLD.email THEN
-		SET NEW.valid_email = 0
-		END IF;
+		SET NEW.valid_email = 0;
+		END IF; 
 	END$$
 
 DELIMITER ;
