95dee2d21e4d9f2c6edd644a8c88265d4e6c18c4
ifeanyi zion chidiebere <ifeanyizion3@gmail.com>
2024-07-10 21:03:52 +0000

    task #6: sql procedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,6 +1,7 @@
 -- SQL script that creates a stored procedure AddBonus that 
 -- adds a new correction for a student.
 
+DELIMITER $$
 
 CREATE PROCEDURE AddBonus 
 	(IN user_id INT,
@@ -14,3 +15,5 @@ CREATE PROCEDURE AddBonus
 		INSERT INTO corrections (user_id, project_id, score)
 		VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 	END$$
+
+DELIMITER ;
