64f0438c3ab811dbe02f12fbe4609ebd61bd7da1
ifeanyi zion chidiebere <ifeanyizion3@gmail.com>
2024-07-10 13:08:08 +0000

    task #0: create table f tables exist on Db

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- create a dabatabase holberton if it exists
+-- creates a table of uniq users
+
+
+CREATE TABLE IF NOT EXISTS holberton.users (
+	id INT AUTO_INCREMENT NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+	);
