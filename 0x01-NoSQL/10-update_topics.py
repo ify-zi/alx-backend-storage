edcb1f4f41a2b13d404ec99c7f5d83f632af50d5
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-15 13:10:48 -0700

    PyMongo: using update_many to alter documents in a python script

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Module that uses python to manipulation the update function
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """function definition of mongodb manip."""
+    filt = {'name': name}
+    u_query = { "$set": {'topics' : topics}}
+    new_doc = mongo_collection.update_many(filt, u_query)
+    return
+
