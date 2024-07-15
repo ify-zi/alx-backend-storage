a4ade3cabb7ee532f032570717d3e353f78ecf09
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-15 13:23:17 -0700

    PyMongo: corrected for PEP8 style

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,7 +7,6 @@
 def update_topics(mongo_collection, name, topics):
     """function definition of mongodb manip."""
     filt = {'name': name}
-    u_query = { "$set": {'topics' : topics}}
+    u_query = {"$set": {'topics': topics}}
     new_doc = mongo_collection.update_many(filt, u_query)
     return
-
