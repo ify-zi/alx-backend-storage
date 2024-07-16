44a2b4349169ff48544e7f78424db89ea3f68fb6
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-16 08:06:42 -0700

    PyMongo: used pymongo module to format nginx logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,17 +7,22 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://127.0.0.1:27017')
 collection = client.logs.nginx
-total = collection.count_documents({})
-get_count = collection.count_documents({"method": "GET"})
-post_count = collection.count_documents({"method": "POST"})
-put_count = collection.count_documents({"method": "PUT"})
-patch_count = collection.count_documents({"method": "PATCH"})
-delete_count = collection.count_documents({"method": "DELETE"})
-stat_count = collection.count_documents({"path": "/status"})
 
-print(f"{total} logs\nMethods:\n\tmethod GET:{get_count}\n\t\
-method POST: {post_count}\n\t\
-method PUT: {put_count}\n\t\
-method PATCH: {patch_count}\n\t\
-method DELETE: {delete_count}\n\
-{stat_count} status check")
+def get_stats(collection):
+    """function definition"""
+    total = collection.count_documents({})
+    get_count = collection.count_documents({"method": "GET"})
+    post_count = collection.count_documents({"method": "POST"})
+    put_count = collection.count_documents({"method": "PUT"})
+    patch_count = collection.count_documents({"method": "PATCH"})
+    delete_count = collection.count_documents({"method": "DELETE"})
+    stat_count = collection.count_documents({"path": "/status"})
+    
+    print(f"{total} logs\nMethods:\n\tmethod GET:{get_count}\n\t\
+    method POST: {post_count}\n\t\
+    method PUT: {put_count}\n\t\
+    method PATCH: {patch_count}\n\t\
+    method DELETE: {delete_count}\n\
+    {stat_count} status check")
+
+get_stats(collection)
