0d4a35ec4d307a726660e5162d0385c785ae84cf
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-17 19:39:32 +0000

    Redis: added get(), get_int()and get_str() method to class cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@
 import uuid
 import random
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -16,10 +16,25 @@ class Cache:
         """definition function"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-        return
 
     def store(self, data: Union[str, bytes, float, int]) -> str:
         """store method declaration"""
         id_key = str(uuid.uuid4())
         self._redis.set(id_key, data)
         return id_key
+
+    def get(self, key: str,
+            fn: Optional[Callable]) -> Union[str, int, bytes, float]:
+        """ return a data from a memor"""
+        data = self._redis.get(key)
+        if key is not None and fn is not None:
+            data = fn(data)
+        return data
+
+    def get_int(self, data: Union[str, int, bytes, float]) -> Union[int, None]:
+        """parametised the data to a format"""
+        return self._redis.get(data)
+
+    def get_str(self, data: Union[str, int, bytes, float]) -> Union[str, None]:
+        """parametise the data to str format"""
+        return self._redis.get(data)
